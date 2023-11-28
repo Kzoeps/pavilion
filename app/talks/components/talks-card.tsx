fd2818b60ae56a47f9758e82cc54e6bfd56d0f8d
kzoeps <kzoepa@gmail.com>
2023-11-28 15:24:05 -0500

    show edit button

--- app/talks/components/talks-card.tsx
+++ app/talks/components/talks-card.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { displayTime } from "@/lib/utils";
+import { Pencil1Icon } from '@radix-ui/react-icons'
+import { Button } from "@/components/ui/button";
 
 interface TalksCardProps {
     title: string;
@@ -13,10 +15,11 @@ export default function TalksCard(props: TalksCardProps) {
     const { title, description, datetime, location, creator_id } = props;
     return (
         <>
-            <Card className=" shadow-md">
+            <Card className=" relative shadow-md">
                 <CardHeader>
                     <CardTitle className=" text-md" >{title}</CardTitle>
                     <CardDescription>{displayTime(datetime)} | {location}</CardDescription>
+                    <Button size="icon" className="absolute -top-4 -right-4 bottom-2" ><Pencil1Icon/></Button>
                 </CardHeader>
                 <CardContent>
                     <p className="text-sm">
--- app/talks/components/talks-display.tsx
+++ app/talks/components/talks-display.tsx
@@ -10,7 +10,7 @@ export default async function TalksDisplay({ year }: TalksDisplayProps) {
     const { rows: talks } = await sql<Talks>`SELECT * FROM talks WHERE EXTRACT(YEAR FROM datetime) = ${filterYear}`
     return (
         <>
-            <section className="grid gap-x-3 gap-y-8 grid-cols-3">
+            <section className="grid gap-x-6 gap-y-8 grid-cols-3">
                 {talks.map((talk) => {
                     return (
                         <TalksCard key={talk.id} title={talk.title} description={talk.description} location={talk.location} datetime={talk.datetime} />
