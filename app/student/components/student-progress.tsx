750cd13a7f8ea33644b10d12aac78daf059f4563
kzoeps <kzoepa@gmail.com>
2023-12-04 00:50:51 -0500

    wip progress bar and display

---
+++ app/student/components/student-progress.tsx
@@ -0,0 +1,56 @@
+const APPROVED_COLOR = 'green-500';
+const UNAPPROVED_COLOR = 'yellow-100';
+const APPROVED_BG_COLOR = `bg-${APPROVED_COLOR}`;
+const UNAPPROVED_BG_COLOR = `bg-${UNAPPROVED_COLOR}`;
+
+const renderProgress = (totalNotes: number, approvedNotes: number) => {
+    return Array.from(Array(10)).map((_, i) => {
+        // if first we need to round since borders are rounded also since approved and unapproved different color we need to handle differently
+        if (i === 0) {
+            if (i < approvedNotes) {
+                return <div key={i} className={`col-span-1 ${APPROVED_BG_COLOR} rounded-s-md`}></div>
+            } else if (i < totalNotes) {
+                return <div key={i} className={`col-span-1 ${UNAPPROVED_BG_COLOR} rounded-s-md`}></div>
+            }
+        } else if (i === 9) {
+            // if last element we need to round right borders
+            if (i < approvedNotes) {
+                return <div key={i} className={`col-span-1 ${APPROVED_BG_COLOR} rounded-e-md`}></div>
+            } else if (i < totalNotes) {
+                return <div key={i} className={`col-span-1 ${UNAPPROVED_BG_COLOR} rounded-e-md`}></div>
+            }
+        } else {
+            // if neither first nor last they are simple rectangles
+            if (i < approvedNotes) {
+                return <div key={i} className={`col-span-1 ${APPROVED_BG_COLOR}`}></div>
+            } else if (i < totalNotes) {
+                return <div key={i} className={`col-span-1 ${UNAPPROVED_BG_COLOR}`}></div>
+            }
+        }
+    })
+}
+
+interface StudentProgressProps {
+    totalNotes: number;
+    approvedNotes: number;
+}
+export default function StudentProgress({ totalNotes, approvedNotes }: StudentProgressProps) {
+    return (
+        <>
+            <h1 className="text-4xl font-bold mt-2">Progress: (<span className={`text-${APPROVED_COLOR}`}>4</span>+<span className={`text-yellow-300`}>2</span>)/10</h1>
+            <section className="flex gap-4 mt-4">
+                <div className="flex items-center gap-3">
+                    <div className={`w-4 h-4 ${APPROVED_BG_COLOR}`} />
+                    <p>Approved Notes</p>
+                </div>
+                <div className="flex items-center gap-3">
+                    <div className={`w-4 h-4 ${UNAPPROVED_BG_COLOR}`} />
+                    <p>Unapproved Notes</p>
+                </div>
+            </section>
+            <div className="w-full grid grid-cols-10 h-6 bg-slate-400 rounded-md mt-2">
+                {renderProgress(8, 4)}
+            </div>
+        </>
+    )
+}
--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,7 +1,12 @@
+import StudentProgress from "./components/student-progress";
+
 export default function Student() {
     return (
         <>
-            <p>Student</p>
+            <h3 className="text-2xl font-medium">Karma Zoepa Yoezer (Class of 2025)</h3>
+            <p className=" italic">kyoezer@conncoll.edu</p>
+            <StudentProgress/>
+
         </>
     )
 }
