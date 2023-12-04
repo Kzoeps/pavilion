20aefd79c9e2d0eac3ed92af1d1eb7715dffed19
kzoeps <kzoepa@gmail.com>
2023-12-04 16:33:48 -0500

    bind with page

--- app/student/[studentId]/page.tsx
+++ app/student/[studentId]/page.tsx
@@ -1,9 +1,23 @@
+import { sql } from "@vercel/postgres";
 import StudentNotesInfo from "../components/student-notes-info";
 
-export default function StudentPage({params}: {params: {studentId: string}}) {
-    const {studentId} = params
+export default async function StudentPage({ params }: { params: { studentId: string } }) {
+    const { studentId } = params
+    let studentInfo = {
+        name: '',
+        email: '',
+        class_year: ''
+    }
+    if (studentId) {
+        const { rows } = await sql`SELECT name, class_year, email FROM users WHERE id = ${studentId}`
+        if ( rows.length) {
+            studentInfo = rows[0] as typeof studentInfo
+        }
+    }
     return (
         <>
+            <h3 className="text-2xl font-medium">{studentInfo.name} (Class of {studentInfo.class_year})</h3>
+            <p className=" italic">{studentInfo.email}</p>
             <StudentNotesInfo id={studentId} />
         </>
     )
