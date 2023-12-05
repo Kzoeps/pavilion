91ea7e2372542df11b59662374f38bb5146a09a6
kzoeps <kzoepa@gmail.com>
2023-12-05 14:50:56 -0500

    remove import

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,12 +1,10 @@
-import StudentProgress from "./components/student-progress";
 
 export default function Student() {
     return (
         <>
             <h3 className="text-2xl font-medium">Karma Zoepa Yoezer (Class of 2025)</h3>
             <p className=" italic">kyoezer@conncoll.edu</p>
-            <StudentProgress/>
-
+            {/* <StudentProgress/> */}
         </>
     )
 }
