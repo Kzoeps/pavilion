3673f249a102a81564cc65818074f5ab6e970de2
kzoeps <kzoepa@gmail.com>
2023-12-05 15:44:43 -0500

    feat: update stuff

--- app/student/components/student-notes-info.tsx
+++ app/student/components/student-notes-info.tsx
@@ -6,12 +6,12 @@ interface StudentNotesInfoProps {
     id: string;
 }
 export default async function StudentNotesInfo({ id }: StudentNotesInfoProps) {
-    const { rows } = await sql`SELECT COUNT(id) FROM notes WHERE student_id = ${id}`
-    const approvedNotes = 0
+    const { rows } = await sql`SELECT COUNT(id), COUNT(approved) AS approved_count FROM notes WHERE student_id = ${id}`
+    const approvedNotes = +rows?.[0]?.approved_count || 0
     const totalNotes = +rows?.[0]?.count || 0
     return (
         <>
-            <h1 className="text-4xl font-bold mt-2">Progress: (<span className={`text-${APPROVED_COLOR}`}>{approvedNotes}</span>+<span className={`text-yellow-300`}>{getUnapprovedNotes(totalNotes, approvedNotes)}</span>)/10</h1>
+            <h1 className="text-4xl font-bold mt-2">Progress: (<span className={`text-green-500`}>{approvedNotes}</span>+<span className={`text-yellow-300`}>{getUnapprovedNotes(totalNotes, approvedNotes)}</span>)/10</h1>
             <section className="flex gap-4 mt-4">
                 <div className="flex items-center gap-3">
                     <div className={`w-4 h-4 bg-${APPROVED_COLOR}`} />
--- app/student/components/student-progress.tsx
+++ app/student/components/student-progress.tsx
@@ -8,23 +8,23 @@ const renderProgress = (totalNotes: number, approvedNotes: number) => {
         // if first we need to round since borders are rounded also since approved and unapproved different color we need to handle differently
         if (i === 0) {
             if (i < approvedNotes) {
-                return <div key={i} className={`col-span-1 ${APPROVED_BG_COLOR} rounded-s-md`}></div>
+                return <div key={i} className={`col-span-1 bg-green-500 rounded-s-md`}></div>
             } else if (i < totalNotes) {
-                return <div key={i} className={`col-span-1 ${UNAPPROVED_BG_COLOR} rounded-s-md`}></div>
+                return <div key={i} className={`col-span-1 bg-yellow-200 rounded-s-md`}></div>
             }
         } else if (i === 9) {
             // if last element we need to round right borders
             if (i < approvedNotes) {
-                return <div key={i} className={`col-span-1 ${APPROVED_BG_COLOR} rounded-e-md`}></div>
+                return <div key={i} className={`col-span-1 bg-green-500 rounded-e-md`}></div>
             } else if (i < totalNotes) {
-                return <div key={i} className={`col-span-1 ${UNAPPROVED_BG_COLOR} rounded-e-md`}></div>
+                return <div key={i} className={`col-span-1 bg-yellow-200 rounded-e-md`}></div>
             }
         } else {
             // if neither first nor last they are simple rectangles
             if (i < approvedNotes) {
-                return <div key={i} className={`col-span-1 ${APPROVED_BG_COLOR}`}></div>
+                return <div key={i} className={`col-span-1 bg-green-500`}></div>
             } else if (i < totalNotes) {
-                return <div key={i} className={`col-span-1 ${UNAPPROVED_BG_COLOR}`}></div>
+                return <div key={i} className={`col-span-1 bg-yellow-200`}></div>
             }
         }
     })
@@ -34,9 +34,9 @@ interface StudentProgressProps {
     id: string;
 }
 export default async function StudentProgress({ id }: StudentProgressProps) {
-    const { rows } = await sql`SELECT COUNT(id) FROM notes WHERE student_id = ${id}`
+    const { rows } = await sql`SELECT COUNT(id), COUNT(approved) AS approved_count FROM notes WHERE student_id = ${id}`
     const totalNotes = +rows?.[0]?.count || 0;
-    const approvedNotes = 0;
+    const approvedNotes = +rows?.[0]?.approved_count || 0;
     return (
         <>
             <div className="w-full grid grid-cols-10 h-6 bg-slate-400 rounded-md mt-2">
