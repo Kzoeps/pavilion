db448aee9f3a0506f0e6e6b838ee60e641bfecd5
kzoeps <kzoepa@gmail.com>
2023-12-05 22:03:49 -0500

    feat: update landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Wave from '@/public/sine.png'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <section>
-        {/* <h1 className={cn("scroll-m-20 text-8xl font-extrabold tracking-tight pb-3 lg:text-5xl text-transparent text-center bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500")}>Easily Manage <br/>Your COM 499 Requirements</h1> */}
-        <h1 className={cn("scroll-m-20 text-8xl font-extrabold tracking-tight pb-3 lg:text-5xl text-left")}>Easily Manage <br />COM 499 Reqs</h1>
-        <h3 className={cn("text-lg font-bold text-gray-500 w-2/5  text-left")}>Pavilion is an all in one platform that makes managing COM 499 requirements easy for admins and taking notes easier for students</h3>
+    <main className="flex min-h-screen flex-col items-center p-24 pt-12">
+      <section className='w-3/5 flex' >
+        <div className='w-2/5'>
+          <h1 className={cn("scroll-m-20 text-8xl font-bold tracking-wide pb-3 lg:text-5xl text-left font-archivo")}>Easily Manage <br />COM 499 Reqs</h1>
+          <h3 className={cn("text-lg font-bold text-gray-500  text-left")}>Pavilion is an all in one platform that makes managing COM 499 requirements easy for admins and taking notes easier for students</h3>
+        </div>
+        <Image src={Wave} alt='waves'/>
       </section>
-      <section className='mt-8 text-left w-full'>
+      <section className='mt-12 text-left w-3/5'>
         <h2 className={cn("text-4xl font-bold text-left")}>Features</h2>
         <div className="grid grid-cols-3 gap-12">
           <div className=' col-span-1 border border-solid border-gray-400 rounded-md p-4 mt-8' >
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ const config: Config = {
       },
     },
     extend: {
+      fontFamily: {
+        archivo: ["var(--font-archivo-black)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
